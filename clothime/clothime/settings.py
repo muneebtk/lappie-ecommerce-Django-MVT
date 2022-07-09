6b91834a2bc0b02fd79c14d66bd2f70ec3ca781b
muhammed muneeb <103311072+muneebtk@users.noreply.github.com>
2022-07-09 17:15:50 +0530

    drop

--- clothime/clothime/settings.py
+++ clothime/clothime/settings.py
@@ -88,11 +88,11 @@ AUTH_USER_MODEL = 'user.user'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('NAME'),
-        'USER': config('USER'),
-        'PASSWORD': config('PASSWORD'),
-        'HOST': config('HOST'),
-        'PORT':config('PORT'),
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT':config('DB_PORT'),
        
     }
 }

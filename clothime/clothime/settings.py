42eeee3a921297aa35e2193d1bc04689586b9fc1
muhammed muneeb <103311072+muneebtk@users.noreply.github.com>
2023-09-11 11:58:13 +0530

    cors setup done

--- clothime/clothime/settings.py
+++ clothime/clothime/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'store',
     'orders',
     'admin_panel',
+    'corsheaders'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -50,11 +51,15 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+CORS_ALLOWED_ORIGINS = [
+    "http://54.163.86.221",  # Specify the allowed origin(s)
 ]
 
 ROOT_URLCONF = 'clothime.urls'
